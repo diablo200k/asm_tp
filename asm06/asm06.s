8c6d80a12786ad7263861d9bfeb71b4c7013bbf5
Mohamed Hassan <mhassan4@myges.fr>
2025-09-29 18:08:31 +0200

    change 06

--- asm06/asm06.s
+++ asm06/asm06.s
@@ -1,116 +1,110 @@
 section .bss
-buf resb 20
+    outbuf  resb 32
+
+section .data
+    nl db 10
+    minus db '-'
 
 section .text
 global _start
 
+atoi_signed:
+    xor     rax, rax
+    xor     r8,  r8
+    mov     dl, [rdi]
+    cmp     dl, '+'
+    jne     .chkminus
+    inc     rdi
+    jmp     .loop
+.chkminus:
+    cmp     dl, '-'
+    jne     .loop
+    inc     rdi
+    mov     r8, 1
+.loop:
+    mov     dl, [rdi]
+    test    dl, dl
+    jz      .done
+    cmp     dl, 10
+    je      .done
+    cmp     dl, '0'
+    jb      .done
+    cmp     dl, '9'
+    ja      .done
+    imul    rax, rax, 10
+    sub     dl, '0'
+    movzx   rdx, dl
+    add     rax, rdx
+    inc     rdi
+    jmp     .loop
+.done:
+    test    r8, r8
+    jz      .ret
+    neg     rax
+.ret:
+    ret
+
+print_i64:
+    mov     rbx, rax
+    test    rbx, rbx
+    jns     .pos
+    mov     rax, 1
+    mov     rdi, 1
+    lea     rsi, [rel minus]
+    mov     rdx, 1
+    syscall
+    mov     rax, rbx
+    neg     rax
+.pos:
+    lea     rsi, [outbuf+32]
+    test    rax, rax
+    jne     .conv
+    mov     byte [outbuf+31], '0'
+    lea     rsi, [outbuf+31]
+    jmp     .write
+.conv:
+    .loop:
+        xor     rdx, rdx
+        mov     rcx, 10
+        div     rcx
+        add     dl, '0'
+        dec     rsi
+        mov     [rsi], dl
+        test    rax, rax
+        jne     .loop
+.write:
+    mov     rdx, outbuf+32
+    sub     rdx, rsi
+    mov     rax, 1
+    mov     rdi, 1
+    syscall
+    mov     rax, 1
+    mov     rdi, 1
+    lea     rsi, [rel nl]
+    mov     rdx, 1
+    syscall
+    ret
+
 _start:
-    mov rax, [rsp]
-    cmp rax, 3
-    jne exit_error
+    mov     rsi, rsp
+    mov     rdi, [rsi]
+    cmp     rdi, 3
+    jl      .noargs
 
-    mov rsi, [rsp+16]
-    mov rdi, [rsp+24]
+    mov     rdi, [rsi+16]
+    call    atoi_signed
+    mov     r12, rax
 
-    xor rax, rax
-    xor rcx, rcx
-    mov r8, 1
-atoi1:
-    mov dl, byte [rsi+rcx]
-    cmp dl, 0
-    je atoi1_done
-    cmp dl, '-'
-    jne atoi1_digit
-    mov r8, -1
-    inc rcx
-    jmp atoi1
-atoi1_digit:
-    sub dl, '0'
-    mov rdx, rax
-    imul rax, rdx, 10
-    add rax, rdx
-    add rax, rdx
-    add rax, rdx
-    add rax, rdx
-    add rax, rdx
-    add rax, rdx
-    add rax, rdx
-    add rax, rdx
-    add rax, rdx
-    add rax, rdx
-    inc rcx
-    jmp atoi1
-atoi1_done:
-    imul rax, r8
-    mov r8, rax
+    mov     rdi, [rsi+24]
+    call    atoi_signed
+    add     rax, r12
 
-    xor rax, rax
-    xor rcx, rcx
-    mov r9, 1
-atoi2:
-    mov dl, byte [rdi+rcx]
-    cmp dl, 0
-    je atoi2_done
-    cmp dl, '-'
-    jne atoi2_digit
-    mov r9, -1
-    inc rcx
-    jmp atoi2
-atoi2_digit:
-    sub dl, '0'
-    mov rdx, rax
-    imul rax, rdx, 10
-    add rax, rdx
-    add rax, rdx
-    add rax, rdx
-    add rax, rdx
-    add rax, rdx
-    add rax, rdx
-    add rax, rdx
-    add rax, rdx
-    add rax, rdx
-    add rax, rdx
-    inc rcx
-    jmp atoi2
-atoi2_done:
-    imul rax, r9
-    add rax, r8
-    mov rbx, rax
-
-    lea rsi, [buf+19]
-    mov r8, rbx
-    cmp rbx, 0
-    jge itoa_positive
-    neg rbx
-    mov byte [buf], '-'
-itoa_positive:
-itoa:
-    xor rdx, rdx
-    mov rax, rbx
-    mov rdi, 10
-    div rdi
-    add dl, '0'
-    dec rsi
-    mov [rsi], dl
-    mov rbx, rax
-    cmp rbx, 0
-    jne itoa
-
-    cmp r8, 0
-    jge print
-    lea rsi, [buf]
-print:
-    mov rdx, buf+20
-    sub rdx, rsi
-    mov rax, 1
-    mov rdi, 1
+    call    print_i64
+    mov     rax, 60
+    xor     rdi, rdi
     syscall
 
-    mov rax, 60
-    xor rdi, rdi
-    syscall
-
-exit_error:
-    mov rax, 60
-    mov rdi, 1
+.noargs:
+    mov     rax, 60
+    mov     rdi, 1
     syscall
