31fe4af7f83b60d1d90d1de451864c915d3f8a38
Mohamed Hassan <mhassan4@myges.fr>
2025-09-29 16:54:39 +0200

    debug erreur 2 caracteurs

--- asm02/asm02.s
+++ asm02/asm02.s
@@ -1,5 +1,5 @@
 section .bss
-    buf resb 3          
+    buf resb 4
 
 section .data
     msg db "1337", 10
@@ -9,37 +9,38 @@ section .text
     global _start
 
 _start:
-    ; read(0, buf, 3)
-    mov rax, 0          
-    mov rdi, 0          
+    mov rax, 0
+    mov rdi, 0
     mov rsi, buf
-    mov rdx, 3
+    mov rdx, 4
     syscall
 
-    ; comparer buf[0] avec '4'
+    cmp rax, 3
+    jne not_42
+
     mov al, byte [buf]
     cmp al, '4'
     jne not_42
 
-    ; comparer buf[1] avec '2'
     mov al, byte [buf+1]
     cmp al, '2'
     jne not_42
 
-    ; si "42", afficher 1337
-    mov rax, 1          
-    mov rdi, 1          
+    mov al, byte [buf+2]
+    cmp al, 10
+    jne not_42
+
+    mov rax, 1
+    mov rdi, 1
     mov rsi, msg
     mov rdx, len
     syscall
 
-    ; exit(0)
     mov rax, 60
     xor rdi, rdi
     syscall
 
 not_42:
-    ; exit(1)
     mov rax, 60
     mov rdi, 1
     syscall
