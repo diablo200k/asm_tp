dc11cc18d94ebe01db97fc83375a7afba444c459
Mohamed Hassan <mhassan4@myges.fr>
2025-09-29 16:55:45 +0200

    debug le A

--- asm04/asm04.s
+++ asm04/asm04.s
@@ -5,27 +5,35 @@ section .text
     global _start
 
 _start:
-    
-    mov rax, 0         
-    mov rdi, 0          
+    mov rax, 0
+    mov rdi, 0
     mov rsi, buf
     mov rdx, 32
-    syscall            
+    syscall
 
-   
     movzx rbx, byte [buf]
-    sub rbx, '0'       
-
+    
+    cmp rbx, '0'
+    jl invalid_input
+    cmp rbx, '9'
+    jg invalid_input
+    
+    sub rbx, '0'
     
     test rbx, 1
     jnz odd
 
 even:
-    mov rax, 60        
-    xor rdi, rdi       
+    mov rax, 60
+    xor rdi, rdi
     syscall
 
 odd:
-    mov rax, 60        
-    mov rdi, 1         
+    mov rax, 60
+    mov rdi, 1
+    syscall
+
+invalid_input:
+    mov rax, 60
+    mov rdi, 2
     syscall
