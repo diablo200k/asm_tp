351bc98eb0631ec5927d6f24bd3a53288c7d549f
Mohamed Hassan <mhassan4@myges.fr>
2025-10-01 16:18:36 +0200

    update asm18

--- asm18/asm18.s
+++ asm18/asm18.s
@@ -1,121 +1,93 @@
-BITS 64
-default rel
-
-%define AF_INET 2
-%define SOCK_DGRAM 2
-%define SOL_SOCKET 1
-%define SO_RCVTIMEO 20
-%define SYS_write 1
-%define SYS_close 3
-%define SYS_socket 41
-%define SYS_sendto 44
-%define SYS_recvfrom 45
-%define SYS_setsockopt 54
-%define SYS_exit 60
-%define STDOUT 1
-%define SERVER_PORT 9999
-%define RCV_TIMEOUT_S 2
-%define IPV4_LOCALHOST 0x0100007F
-
 section .data
+hdr        db 'message: "',0
+hdr_len    equ $-hdr
+tail       db '"',10
+to_msg     db 'Timeout: no response from server',10
+to_len     equ $-to_msg
+timeval    dq 2,0
 sockaddr:
-    dw AF_INET
-    dw 0x8F27          ; 9999 en big endian = 0x270F mais inversé (NASM little) = 0x0F27 -> pour 9999: 0x270F -> donc dw 0x270F
-    dd IPV4_LOCALHOST
+    dw 2
+    dw 0x3905
+    dd 0x0100007F
     dq 0
-timeval:
-    dq RCV_TIMEOUT_S
-    dq 0
-msg_prefix: db 'message: "'
-msg_suffix: db '"',10
-msg_timeout: db 'Timeout: no response from server',10
-ping: db 'ping'
 
 section .bss
-buf resb 1024
+buf  resb 1024
 
 section .text
 global _start
 
 _start:
-    mov rax,SYS_socket
-    mov rdi,AF_INET
-    mov rsi,SOCK_DGRAM
+    mov rax,41
+    mov rdi,2
+    mov rsi,2
     xor rdx,rdx
     syscall
     mov r12,rax
 
-    mov rax,SYS_setsockopt
+    mov rax,42
     mov rdi,r12
-    mov rsi,SOL_SOCKET
-    mov rdx,SO_RCVTIMEO
+    lea rsi,[rel sockaddr]
+    mov rdx,16
+    syscall
+
+    mov rax,54
+    mov rdi,r12
+    mov rsi,1
+    mov rdx,20
     lea r10,[rel timeval]
     mov r8,16
     syscall
 
-    mov rax,SYS_sendto
+    mov byte [buf],'X'
+    mov rax,44
     mov rdi,r12
-    lea rsi,[rel ping]
-    mov rdx,4
+    lea rsi,[rel buf]
+    mov rdx,1
     xor r10,r10
-    lea r8,[rel sockaddr]
-    mov r9,16
+    xor r8,r8
+    xor r9,r9
     syscall
 
-    mov rax,SYS_recvfrom
+    mov rax,45
     mov rdi,r12
     lea rsi,[rel buf]
     mov rdx,1024
     xor r10,r10
     xor r8,r8
-    xor r9,r9
     syscall
     test rax,rax
-    js timeout
+    jle .timeout
     mov r13,rax
 
-    mov rax,SYS_write
-    mov rdi,STDOUT
-    lea rsi,[rel msg_prefix]
-    mov rdx,10          ; longueur "message: \""
+    mov rax,1
+    mov rdi,1
+    lea rsi,[rel hdr]
+    mov rdx,hdr_len
     syscall
 
-    mov rax,SYS_write
-    mov rdi,STDOUT
+    mov rax,1
+    mov rdi,1
     lea rsi,[rel buf]
     mov rdx,r13
     syscall
 
-    mov rax,SYS_write
-    mov rdi,STDOUT
-    lea rsi,[rel msg_suffix]
+    mov rax,1
+    mov rdi,1
+    lea rsi,[rel tail]
     mov rdx,2
     syscall
 
-    mov rax,SYS_close
-    mov rdi,r12
-    syscall
-    mov rax,SYS_exit
+    mov rax,60
     xor rdi,rdi
     syscall
 
-timeout:
-    neg rax
-    cmp rax,11
-    jne fatal
-    mov rax,SYS_write
-    mov rdi,STDOUT
-    lea rsi,[rel msg_timeout]
-    mov rdx,33
-    syscall
-    mov rax,SYS_close
-    mov rdi,r12
-    syscall
-    mov rax,SYS_exit
+.timeout:
+    mov rax,1
     mov rdi,1
+    lea rsi,[rel to_msg]
+    mov rdx,to_len
     syscall
-
-fatal:
-    mov rax,SYS_exit
+    mov rax,60
     mov rdi,1
     syscall
