6a9eaefe155834370125c6021cf2e6c065628c22
Mohamed Hassan <mhassan4@myges.fr>
2025-10-03 15:40:00 +0200

    asm 04 correction

--- asm04/asm04.s
+++ asm04/asm04.s
@@ -11,16 +11,69 @@ _start:
     mov rdx, 32
     syscall
 
-    movzx rbx, byte [buf]
+    test rax, rax
+    jz invalid_input
     
-    cmp rbx, '0'
-    jl invalid_input
-    cmp rbx, '9'
-    jg invalid_input
+    mov rsi, buf
+    xor rcx, rcx
+    xor r8, r8
+    xor r9, r9
     
-    sub rbx, '0'
+    movzx rax, byte [rsi]
+    cmp al, '-'
+    jne .check_plus
+    mov r8, 1
+    inc rsi
+    jmp .parse_loop
     
-    test rbx, 1
+.check_plus:
+    cmp al, '+'
+    jne .parse_loop
+    inc rsi
+
+.parse_loop:
+    movzx rax, byte [rsi]
+    
+    cmp al, 10
+    je .done
+    cmp al, 13
+    je .done
+    test al, al
+    jz .done
+    
+    cmp al, '0'
+    jb invalid_input
+    cmp al, '9'
+    ja invalid_input
+    
+    sub al, '0'
+    
+    mov rdx, rcx
+    imul rdx, 10
+    jo overflow_detected
+    
+    movzx rbx, al
+    add rdx, rbx
+    jo overflow_detected
+    
+    cmp rdx, 2147483647
+    ja overflow_detected
+    
+    mov rcx, rdx
+    inc r9
+    inc rsi
+    jmp .parse_loop
+
+.done:
+    test r9, r9
+    jz invalid_input
+    
+    test r8, r8
+    jz .check_parity
+    neg rcx
+
+.check_parity:
+    test rcx, 1
     jnz odd
 
 even:
@@ -34,6 +87,11 @@ odd:
     syscall
 
 invalid_input:
+    mov rax, 60
+    mov rdi, 2
+    syscall
+
+overflow_detected:
     mov rax, 60
     mov rdi, 2
     syscall
