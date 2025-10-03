90445a55da7a5f607e3b2cffdf9d974835e82526
Mohamed Hassan <mhassan4@myges.fr>
2025-10-03 15:57:24 +0200

    asm13

--- asm13/asm13.s
+++ asm13/asm13.s
@@ -4,49 +4,48 @@ section .bss
 section .text
 global _start
 _start:
-    mov rax, 0
-    mov rdi, 0
-    mov rsi, buffer
-    mov rdx, 1024
+    mov     rax, 0
+    mov     rdi, 0
+    mov     rsi, buffer
+    mov     rdx, 1024
     syscall
-    
-    mov rcx, rax
-    cmp rcx, 0
-    jle .is_palindrome
-    
-    mov rsi, buffer
-    add rsi, rcx
-    dec rsi
-    
-    cmp byte [rsi], 10
-    jne .setup_check
-    dec rsi
-    dec rcx
-    
-.setup_check:
-    mov rdi, buffer
-    mov r8, rsi
-    
+
+    mov     rcx, rax
+    cmp     rcx, 0
+    jle     .palindrome
+
+    mov     rsi, buffer
+    add     rsi, rcx
+    dec     rsi
+
+    cmp     byte [rsi], 10
+    jne     .skip_nl
+    dec     rsi
+    dec     rcx
+.skip_nl:
+
+    mov     rdi, buffer
+    mov     r8, rsi
+
 .check_loop:
-    cmp rdi, r8
-    jge .is_palindrome
-    
-    mov al, [rdi]
-    mov bl, [r8]
-    
-    cmp al, bl
-    jne .not_palindrome
-    
-    inc rdi
-    dec r8
-    jmp .check_loop
-    
-.is_palindrome:
-    mov rax, 60
-    xor rdi, rdi
+    cmp     rdi, r8
+    jge     .palindrome
+
+    mov     al, [rdi]
+    mov     bl, [r8]
+    cmp     al, bl
+    jne     .not_palindrome
+
+    inc     rdi
+    dec     r8
+    jmp     .check_loop
+
+.palindrome:
+    mov     rax, 60
+    xor     rdi, rdi
     syscall
-    
+
 .not_palindrome:
-    mov rax, 60
-    mov rdi, 1
+    mov     rax, 60
+    mov     rdi, 1
     syscall
