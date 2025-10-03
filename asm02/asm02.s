fcee788aabe7dcc12edffb038f936fd4f83cc284
Mohamed Hassan <mhassan4@myges.fr>
2025-10-03 15:38:24 +0200

    asm2 correction

--- asm02/asm02.s
+++ asm02/asm02.s
@@ -1,5 +1,5 @@
 section .bss
-    buf resb 4
+    buf resb 64
 
 section .data
     msg db "1337", 10
@@ -9,15 +9,18 @@ section .text
     global _start
 
 _start:
+    ; Lire l'entrée (jusqu'à 64 octets)
     mov rax, 0
     mov rdi, 0
     mov rsi, buf
-    mov rdx, 4
+    mov rdx, 64
     syscall
 
+    ; Vérifier si on a lu au moins 3 octets
     cmp rax, 3
-    jne not_42
-
+    jl not_42
+    
+    ; Vérifier si c'est "42\n" ou "42\r\n"
     mov al, byte [buf]
     cmp al, '4'
     jne not_42
@@ -27,20 +30,32 @@ _start:
     jne not_42
 
     mov al, byte [buf+2]
+    cmp al, 10          ; '\n'
+    je is_42
+    
+    cmp al, 13          ; '\r'
+    jne not_42
+    
+    ; Vérifier si c'est '\r\n'
+    mov al, byte [buf+3]
     cmp al, 10
     jne not_42
 
+is_42:
+    ; Afficher "1337\n"
     mov rax, 1
     mov rdi, 1
     mov rsi, msg
     mov rdx, len
     syscall
 
+    ; Exit 0
     mov rax, 60
     xor rdi, rdi
     syscall
 
 not_42:
+    ; Exit 1
     mov rax, 60
     mov rdi, 1
     syscall
